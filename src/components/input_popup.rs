c28d47f5ae916ff355a60bc797b273d4a89b4669
bmehdi777 <mehdi.bentouati76@gmail.com>
2024-05-02 13:43:53 +0200

    feat: add moving cursor on input

--- src/components/input_popup.rs
+++ src/components/input_popup.rs
@@ -1,8 +1,8 @@
 use super::MutableComponent;
 use crate::{
     application::Store,
+    components::LayoutArea,
     events::{key::Keys, EventState},
-    components::LayoutArea
 };
 
 use ratatui::{prelude::*, widgets::*};
@@ -18,17 +18,25 @@ pub struct InputPopupComponent {
     pub title: String,
     pub content: String,
     pub action: InputAction,
+    pub cursor_pos: usize,
 }
 
 impl InputPopupComponent {
     pub fn new(title: String, content: String, action: InputAction) -> Self {
-        InputPopupComponent { title, content, action }
+        let cursor_pos = content.len();
+        InputPopupComponent {
+            title,
+            content,
+            action,
+            cursor_pos,
+        }
     }
     pub fn default() -> Self {
         InputPopupComponent {
             title: String::new(),
             content: String::new(),
-            action: InputAction::Insert
+            action: InputAction::Insert,
+            cursor_pos: 0,
         }
     }
 }
@@ -36,8 +44,24 @@ impl InputPopupComponent {
 impl MutableComponent for InputPopupComponent {
     fn event(&mut self, input: &Keys, store: &mut Store) -> anyhow::Result<EventState> {
         match input {
+            Keys::ArrowLeft => {
+                if self.cursor_pos > 0 {
+                    self.cursor_pos -= 1;
+                }
+            }
+            Keys::ArrowRight => {
+                if self.cursor_pos < self.content.len() {
+                    self.cursor_pos += 1;
+                }
+            }
             Keys::Backspace => {
-                self.content.pop();
+                if self.cursor_pos == self.content.len() {
+                    self.content.pop();
+                    self.cursor_pos = self.content.len();
+                } else {
+                    self.content.remove(self.cursor_pos - 1);
+                    self.cursor_pos -= 1;
+                }
             }
             Keys::Enter => {
                 if self.content.trim().len() == 0 {
@@ -49,7 +73,21 @@ impl MutableComponent for InputPopupComponent {
                 return event;
             }
             Keys::Char(c) => {
-                self.content.push_str(&c.to_string());
+                if self.cursor_pos == self.content.len() {
+                    self.content.push_str(&c.to_string());
+                    self.cursor_pos = self.content.len();
+                } else if self.cursor_pos == 0 {
+                    self.content = format!("{}{}", c, self.content);
+                    self.cursor_pos += 1;
+                } else {
+                    self.content = format!(
+                        "{}{}{}",
+                        &self.content[..self.cursor_pos],
+                        c,
+                        &self.content[self.cursor_pos..]
+                    );
+                    self.cursor_pos += 1;
+                }
             }
             Keys::Esc => {
                 return Ok(EventState::Escaped);
@@ -64,7 +102,7 @@ impl MutableComponent for InputPopupComponent {
         area: Rect,
         _selected: bool,
         store: &Store,
-        _layout: &LayoutArea
+        _layout: &LayoutArea,
     ) -> anyhow::Result<()> {
         let container = Block::default()
             .title(&*self.title)
@@ -80,7 +118,7 @@ impl MutableComponent for InputPopupComponent {
             .style(Style::new().fg(self.get_color(store.config.theme_config.unselected_color)));
 
         frame.render_widget(Clear, area);
-        frame.set_cursor(area.x + 2 + self.content.len() as u16, area.y + 1);
+        frame.set_cursor(area.x + 2 + self.cursor_pos as u16, area.y + 1);
         frame.render_widget(text, area);
         Ok(())
     }
